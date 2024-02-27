a3a79f190ba5d8131d3174dd3c04a0e8f0532793
Maltek13 <161490027+Maltek13@users.noreply.github.com>
2024-02-27 16:43:11 +0100

    Add files via upload

---
+++ jawa.js
@@ -0,0 +1,7 @@
+function zmiana(){
+    const kolory = document.getElementById("kolor")
+    document.body.style.backgroundColor = kolory.value
+}
+function nawi(href){
+    window.location.href = href
+}
