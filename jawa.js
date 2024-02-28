8410e60bda3e9e4d848aa0ce5bd8003add442111
Maltek13 <161490027+Maltek13@users.noreply.github.com>
2024-02-28 18:40:20 +0100

    Update jawa.js

--- jawa.js
+++ jawa.js
@@ -1,7 +1,7 @@
 function zmiana(){
     const kolory = document.getElementById("kolor")
     document.body.style.backgroundColor = kolory.value
-    alert("Zmieniono)
+    wimdow.alert("Zmieniono")
 }
 function nawi(href){
     window.location.href = href
